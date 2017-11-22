1d51f246336d045ef75d4f59af1184f5ed5f8553
yowcow <yowcow@cpan.org>
2017-11-22 12:27:22 +0000

    refactor the rest

--- examples/combination.go
+++ examples/combination.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"net/http"
 	"sync"
 
 	"github.com/labstack/gommon/log"
@@ -18,117 +17,57 @@ var feedUrls = []string{
 	"https://news.yahoo.co.jp/pickup/rss.xml",
 }
 
-func httpQueue(workers int, logger *log.Logger) httpworker.Queue {
-	q := httpworker.Queue{
-		Wg:     &sync.WaitGroup{},
-		In:     make(chan *httpworker.RSSFeed),
-		Out:    make(chan *httpworker.RSSFeed),
-		Logger: logger,
-		Client: &http.Client{},
-	}
-	for i := 0; i < workers; i++ {
-		q.Wg.Add(1)
-		go q.Start(i + 1)
-	}
-	return q
-}
-
-func rssQueue(workers int, in chan *httpworker.RSSFeed, logger *log.Logger) rssworker.Queue {
-	q := rssworker.Queue{
-		Wg:     &sync.WaitGroup{},
-		In:     in,
-		Out:    make(chan *rssworker.RSSItem),
-		Logger: logger,
-	}
-	for i := 0; i < workers; i++ {
-		q.Wg.Add(1)
-		go q.Start(i + 1)
-	}
-	return q
-}
-
-func logQueue(workers int, logger *log.Logger) itemworker.Queue {
-	q := itemworker.Queue{
-		Wg: &sync.WaitGroup{},
-		In: make(chan *rssworker.RSSItem),
-		Task: func(item *rssworker.RSSItem) bool {
-			logger.Infof("Link: %s, Title: %s", item.Link, item.Title)
-			return false
-		},
-	}
-	for i := 0; i < workers; i++ {
-		q.Wg.Add(1)
-		go q.Start(i + 1)
-	}
-	return q
-}
-
-func countQueue(workers int, count *int) itemworker.Queue {
-	mx := &sync.Mutex{}
-	q := itemworker.Queue{
-		Wg: &sync.WaitGroup{},
-		In: make(chan *rssworker.RSSItem),
-		Task: func(item *rssworker.RSSItem) bool {
-			mx.Lock()
-			defer mx.Unlock()
-			*count++
-			return false
-		},
-	}
-	for i := 0; i < workers; i++ {
-		q.Wg.Add(1)
-		go q.Start(i + 1)
-	}
-	return q
-}
-
-func broadcasterQueue(workers int, in chan *rssworker.RSSItem, outs ...chan *rssworker.RSSItem) broadcaster.Queue {
-	q := broadcaster.Queue{
-		Wg:   &sync.WaitGroup{},
-		In:   in,
-		Outs: outs,
-	}
-	for i := 0; i < workers; i++ {
-		q.Wg.Add(1)
-		go q.Start(i + 1)
-	}
-	return q
-}
-
 func main() {
-	count := 0
-
-	logbuf := bytes.Buffer{}
+	logbuf := new(bytes.Buffer)
 	logger := log.New("")
-	logger.SetOutput(&logbuf)
+	logger.SetOutput(logbuf)
 	logger.SetHeader(`${level}`)
 
-	httpQueue := httpQueue(4, logger)
-	rssQueue := rssQueue(4, httpQueue.Out, logger)
+	// seed into http queue
+	httpin := make(chan *httpworker.RSSFeed)
 
-	logQueue := logQueue(2, logger)
-	countQueue := countQueue(2, &count)
+	// http worker queue
+	httpqueue := httpworker.New(logger)
+	httpout := httpqueue.Start(httpin, 4)
 
-	broadcasterQueue := broadcasterQueue(2, rssQueue.Out, logQueue.In, countQueue.In)
+	// rss worker queue
+	rssqueue := rssworker.New(logger)
+	rssout := rssqueue.Start(httpout, 4)
 
+	// broadcaster queue
+	bcastqueue := broadcaster.New(2)
+	bcastout := bcastqueue.Start(rssout, 4)
+
+	// logging queue
+	logqueue := itemworker.New("logging", func(item *rssworker.RSSItem) bool {
+		logger.Infof("Link: %s, Title: %s", item.Link, item.Title)
+		return false
+	}, logger)
+	logqueue.Start(bcastout[0], 4)
+
+	// counting queue
+	count := 0
+	mx := new(sync.Mutex)
+	countqueue := itemworker.New("counting", func(item *rssworker.RSSItem) bool {
+		mx.Lock()
+		defer mx.Unlock()
+		count++
+		return false
+	}, logger)
+	countqueue.Start(bcastout[1], 4)
+
+	// now seed to cascade
 	for _, url := range feedUrls {
-		httpQueue.In <- &httpworker.RSSFeed{url, nil, nil}
+		httpin <- &httpworker.RSSFeed{url, nil, nil}
 	}
 
-	close(httpQueue.In)
-	httpQueue.Wg.Wait()
-
-	close(rssQueue.In)
-	rssQueue.Wg.Wait()
-
-	close(broadcasterQueue.In)
-	broadcasterQueue.Wg.Wait()
-
-	close(logQueue.In)
-	logQueue.Wg.Wait()
-
-	close(countQueue.In)
-	countQueue.Wg.Wait()
+	// cascade close
+	close(httpin)
+	httpqueue.Finish()
+	rssqueue.Finish()
+	bcastqueue.Finish()
+	logqueue.Finish()
+	countqueue.Finish()
 
 	fmt.Println("Fetched total =>", count)
 	fmt.Println(logbuf.String())
